8d613aa7eee3547a281b76e63afba131cfffac68
NastyaShapenkova <wondernasty@yandex.ru>
2024-07-08 17:37:58 +0500

    make a swiper

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -275,6 +275,25 @@
 	});
    }
 
-   //
-   
+   //слайдер-детальная страница
+   const swipers = document.querySelectorAll(".js-swiper");
+      swipers.forEach(function (swpr) {
+         new Swiper(swpr, {
+            updateOnWindowResize: true,
+            slidesPerView: "auto",
+            freeMode: true,
+            spaceBetween: 0,
+            speed: 500,
+            grabCursor: true,
+            pagination: {
+               el: ".swiper-pagination",
+               clickable: true
+         },
+         navigation: {
+            nextEl: ".swiper-arrow-next",
+            prevEl: ".swiper-arrow-prev",
+            disabledClass: "arrow--disabled"
+         }
+      });
+   });
 }) ();
