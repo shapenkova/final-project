17c80140e1fdb01acb716d43af4d17dea6a6891e
NastyaShapenkova <wondernasty@yandex.ru>
2024-07-11 17:47:11 +0500

    make a form

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -326,4 +326,42 @@
          eventPP.addEventListener("click", closeEventPP);
       });
    }
+   //маска для телефона
+   const mobileMask = $('.js-mobileMask');
+   if (mobileMask.length) {
+      mobileMask.mask('+7 (000) 000 00 00', { placeholder: "+7 (___) ___ __ __" });
+   }
+   //селект
+   const jsSelectric = $(".js-selectric");
+   if (jsSelectric.length) {
+      jsSelectric.selectric({
+         nativeOnMobile: false
+   });
+   //календарь
+   const dateField = $(".js-dateField");
+if (dateField.length) {
+  const pickerInit = function (pick) {
+    const dateInput = pick.find(".js-dateInput");
+    const dateDay = pick.find(".js-dateDay");
+    const dateMonth = pick.find(".js-dateMonth");
+    const dateYear = pick.find(".js-dateYear");
+    const dateConfig = {
+      autoClose: true,
+      minDate: new Date(),
+      navTitles: {
+        days: "MMMM <i>yyyy</i>"
+      },
+      onSelect: function ({ date }) {
+        dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+        dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+        dateYear.val(date ? date.getFullYear() : "");
+      }
+    };
+    new AirDatepicker(dateInput[0], dateConfig);
+  };
+  $.each(dateField, function (i) {
+    pickerInit($(this));
+  });
+}
+}
 }) ();
