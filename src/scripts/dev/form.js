72e510bb58c917b0123a8126a72835513ce45240
NastyaShapenkova <wondernasty@yandex.ru>
2024-07-21 19:59:00 +0500

    adaptive

---
+++ src/scripts/dev/form.js
@@ -0,0 +1,79 @@
+(function() {
+   "use strict";
+   //маска для телефона
+   const mobileMask = $('.js-mobileMask');
+   if (mobileMask.length) {
+      mobileMask.mask('+7 (000) 000 00 00', { placeholder: "+7 (___) ___ __ __" });
+   }
+   
+   //селект     
+   const jsSelectric = $(".js-selectric");
+   if (jsSelectric.length) {
+         jsSelectric.selectric({
+         nativeOnMobile: false,
+         onOpen: function() {
+            $('.custom-select').addClass('open');
+            $('.custom-title').addClass('open2');
+            
+         },
+         onClose: function() {
+            $('.custom-select').removeClass('open');
+            $('.custom-title').removeClass('open2');
+         }
+      });
+   } 
+
+   $.get('ajax.html', function(data) {
+      $('#ajax').append(data).selectric({
+         maxHeight: 200
+      }
+      );
+   });
+   $('.js-selectric').on('selectric-init', function() {
+      const scrollElement = $('.selectric-scroll')[0];
+      if (scrollElement) {
+         new SimpleBar(scrollElement);
+      }
+   }).selectric('refresh');
+   
+   $('.custom-select select').on('selectric-open', function() {
+      $(this).closest('.selectric-wrapper').addClass('open3');
+   });
+   $('.custom-select select').on('selectric-close', function() {
+      $(this).closest('.selectric-wrapper').removeClass('open3');
+   });
+   $('select').on('selectric-open', function() {
+      $(this).closest('.selectric-wrapper').addClass('open4');
+   }).on('selectric-close', function() {
+      $(this).closest('.selectric-wrapper').removeClass('open4');
+   });
+
+
+   //календарь
+   const dateField = $(".js-dateField");
+   if (dateField.length) {
+      const pickerInit = function (pick) {
+         const dateInput = pick.find(".js-dateInput");
+         const dateDay = pick.find(".js-dateDay");
+         const dateMonth = pick.find(".js-dateMonth");
+         const dateYear = pick.find(".js-dateYear");
+         const dateConfig = {
+            autoClose: true,
+            navTitles: {
+            days: "MMMM <i>yyyy</i>",
+            
+         },
+         onSelect: function ({ date }) {
+            dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+            dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+            dateYear.val(date ? date.getFullYear() : "");
+         }
+      };
+      new AirDatepicker(dateInput[0], dateConfig);
+      
+      };
+      $.each(dateField, function (i) {
+         pickerInit($(this));
+      });
+      }
+})()
